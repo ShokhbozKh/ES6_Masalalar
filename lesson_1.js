2725b43229c8434b31322f37ea7811aa0929a09b
ShokhbozKh <117597489+ShokhbozKh@users.noreply.github.com>
2022-11-07 23:17:46 +0500

    Add files via upload

---
+++ lesson_1.js
@@ -0,0 +1,107 @@
+////Kiritish chiqarish va o'zlashtirish operatorlari
+//#1
+// let a=4,p;
+// p=4*a;
+// console.log('p:',p);
+//#2
+// let a=4,s;
+// s = a*a;
+// console.log('s:',s);
+//#3
+// let a=4,b=4,s,p;
+// s=a*b;
+// p=2*(a+b);
+// console.log('s:',s,' p:',p);
+//#4
+// let d=4,l;
+// const pi=3.14;
+// l=pi*d;
+// console.log('l:',l);
+//#5
+// let a=5,s,v;
+// s=6*a*a;
+// v=a*a*a;
+// console.log('s:',s,' v:',v);
+//#6
+// let a=1,b=2,c=3,s,v;
+// v=a*b*c;
+// s=2*(a*b+b*c+a*c);
+// console.log('v:',v,'s:',s);
+//#7
+// let r=6;
+// let l,s;
+// l=2*Math.PI*r;
+// s=Math.PI*r*r;
+// console.log('l:',l,' s:',s);
+//#8
+// let a=4,b=7,c;
+// c=(a+b)/2 ;
+// console.log('urta arfimetik:',c);
+//#9
+// let a=5,b=9,g;
+// g=Math.sqrt(a*b);
+// console.log('urta geometrik:',g);
+//#10
+// let a=3,b=5;
+// console.log('yig"ini:',a+b);
+// console.log('kupaytmasi:',a*b,' ','a*a:',a*a,' b*b:',b*b);
+//#11
+// let a=-9,b=4;
+// console.log('yigindi:',a+b,'kupaytma:',a*b,'a modul:',Math.abs(a),' b modul:',Math.abs(b));
+//#12
+// let a=2,b=4,c,p;
+// c=Math.sqrt(a*a+b*b);
+// p=a+b+c;
+// console.log('c:',c,' p:',p);
+//13
+// let r1=2,r2=4,s1,s2,s3;
+// const pi=3.1415;
+// s1=pi*r1;
+// s2=pi*r2;
+// s3=pi*(r1-r2);
+// console.log('s1:',s1,'s2:',s2,'s3:',s3);
+//#14
+// let l=12,r,s;
+// const pi=3.14;
+// r=(l)/2*pi;
+// s=pi*r*r;
+// console.log('r:',r,' s:',s);
+//#15
+// let s=14,d,r;
+// const pi=3.14;
+// r=Math.sqrt(s/pi);
+// d=2*r;
+// console.log('r:',r,'d:',d);
+//#16
+// let a1=34,a2=45;
+// console.log('masofa:',a2-a1);
+//#17
+// let a=2,b=3,c=4;
+// console.log('ac:',c-a,' bc:',c-b,'umumiy kesma:',Math.abs(a-c)+Math.abs(b-a)+Math.abs(b-c));
+//#18
+// let a=4,b=7,c=6;
+// //a---c---b
+// let ac=c-a;
+// let bc=c-b;
+// console.log('ac*bc:',Math.abs(ac*bc));
+//#19
+// let x1=2, x2=6;
+// let y1=3, y2=9;
+// let a=Math.abs(x2-x1), b=Math.abs(y2-y1);
+// let s,p;
+// s=a*b;
+// p=2*(a+b);
+// console.log('yuzi:',s,'peemetr:',p);
+//#20
+// let x1=5,x2=1;
+// let y1=5,y2=2;
+// let l=Math.sqrt(Math.pow(x2-x1,2) + Math.pow(y2-y1,2));
+// console.log("uzunligi:",l);
+
+
+
+
+
+
+
+
