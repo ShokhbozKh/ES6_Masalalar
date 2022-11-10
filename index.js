3e2c3b8d6e6d35f7464161b1a871c923c0ac3309
Shohboz Xurramov <117597489+ShokhbozKh@users.noreply.github.com>
2022-11-10 23:37:56 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,207 @@
+// 2 masalalar Butun sonlarga oid 
+//#1
+// let l=1247;
+// let m=Math.round( l/100);
+// console.log(m);
+
+//#2
+// let m=24009,t;
+// t=m/1000;
+// console.log(Math.floor(t));
+
+//#3
+// let h=2047746,kb;
+// kb=Math.floor(h/1024);
+// console.log(kb);
+
+//#4
+// let a=8,b=3;
+// let n=Math.floor(a/b);
+// console.log(n);
+
+//#5
+// let a=12,b=7;
+// let n=Math.floor(a/b);
+// let nx=(a%b);
+// console.log(n);
+// console.log(nx);
+
+//#6
+// let a=24;
+// let un=Math.floor(a/10);
+// let bir=a%10;
+// console.log(un,' ',bir);
+
+//#7
+// let a=67;
+// let yig=Math.floor(a/10)+a%10 ;
+// console.log(yig);
+
+//#8
+// let a=59;
+// console.log(a%10,Math.floor(a/10));
+
+//#9
+// let a=987;
+// console.log('yuzlar xonasi:',Math.floor(a/100));
+
+//#10
+// let a=235;
+// let b=a%10;
+// let un=Math.floor((a%100) / 10);
+// console.log(b,' ',un);
+
+//#11
+// let a=901;
+// let b=a%10,u=(Math.floor(a/10)%10),y=Math.floor(a/100);
+// console.log(b+u+y);
+
+//#12
+// let a=123;
+// let t1=Math.floor(a/100),t2=Math.floor(a/10)%10,t3=a%10;
+// console.log(t3,t2,t1);
+
+//#13
+// let a=345;
+// let a1=Math.floor(a/100);
+// let a2=Math.floor(a/10)%10;
+// let a3=a%10;
+// console.log(a2,a3,a1);
+
+//#14
+// let a=765;
+// let a1=Math.floor(a/100);
+// let a2=Math.floor(a/10)%10;
+// let a3=a%10;
+// console.log(a3,a1,a2);
+
+//#15
+// let a=123;
+// let a1,a2,a3;
+// a1=Math.floor(a/100);
+// a2=Math.floor(a/10)%10;
+// a3=a%10;
+// console.log(a1,a3,a2);
+
+//#16
+// let a=123;
+// let a1=Math.floor(a/100);
+// let a2=Math.floor(a/10)%10;
+// let a3=Math.floor(a%10);
+// console.log(a1,a3,a2);
+
+//#17
+// let a=2345;
+// let y=Math.floor(a/100)%10;
+// console.log(y);
+
+//#18
+// let a=4579;
+// let m=Math.floor(a/1000)%10;
+// console.log('y:',m);
+
+//#19
+// let n=12040;
+// let hours= Math.floor(n/60);
+// let second=n%60;
+// console.log('minut:',hours,'sekund:'+ second);
+
+//#20
+// let n=243456;
+// let hours=Math.floor(n/3600);
+// console.log(hours);
+
+//#21
+// let n=34568;
+// let min=Math.floor(n/60);
+// let sec=n%60 ;
+// console.log('minut:',min, 'minut va ',sec,'secund utdi');
+
+//#22
+// let n=67890;
+// let h=Math.floor(n/3600);
+// let s=n%60;
+// console.log(h,' soat va',s,'secund utdi.');
+
+//#23
+// let n=345671;
+// let h,m,s;
+// h=Math.floor(n/3600);
+// m=Math.floor(n/60);
+// s=n%60;
+// console.log(h,'soat',m,'minut',s,'sekund utdi');
+
+//#24
+// let k=7;
+// if (k>0 && k<366){
+//     if(k%7 ==0){
+//         console.log('Yakshanba');
+//     }else if(k%7==1){
+//         console.log('Dushanba');
+//     }else if(k%7==2){
+//         console.log('Seshanba');
+//     }else if(k%7==3){
+//         console.log('Chorshanba');
+//     }else if(k%7==4){
+//         console.log('Payshanba');
+//     }else if(k%7==5){
+//         console.log('Juma');
+//     }else if(k%7==6){
+//         console.log('Shanba');
+//     }
+// }else{
+//     console.log('kiritilgan kun xato');
+// }
+
+//#25
+// let k=1;
+// if( k%7==1 ){
+//     console.log('Payshanba');
+// }else if( k%7==2 ){
+//     console.log('Juma');
+// }else if(k%7==3){
+//     console.log('Shanba');
+// }else if ( k%7==4 ){
+//     console.log('Yakshanba');
+// }else if( k%7==5 ){
+//     console.log('Dushanba');
+// }else if(k%7==6){
+//     console.log('Seshanba');
+// }else if(k%7==7){
+//     console.log('Chorshanba');
+// }
+
+//#26
+// const k=77;
+// day=((k%7)+1);
+// console.log('hozir xaftani:',day,'kuni');
+
+//#27
+// const k=8;
+// day=((k+5)%7+1);
+// console.log('hozir xaftaning :',day,'kuni');
+
+//#28
+// const k=1;
+// n=3;
+// let day=((k+n-2)%7)+1;
+// console.log('hozirgi kun:',day);
+
+
+//#29
+// let a=4,b=6,c=3;
+// let n,s1,s2,sn;
+// s1=a*b;
+// s2=c*c;
+// n=Math.floor(s1/s2);
+// sn=s1-n*c*c;
+// console.log("n=",n,'ta s=',sn);
+
+//#30
+// let y=1903;
+// let t=Math.floor(y/100)+1;
+// console.log(t);
+
+
+
+
