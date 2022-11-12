c96fbcf8e5240176931fb5345920f0f02187b621
Shohboz Xurramov <117597489+ShokhbozKh@users.noreply.github.com>
2022-11-12 21:47:53 +0500

    Add files via upload

---
+++ string_m.js
@@ -0,0 +1,84 @@
+//#1
+// let kocha="Bog'bon";
+// let mahalla="Sag'bon";
+// let tuman="Bodomzor";
+// let viloyat="Samarqand";
+// let natija=`${kocha} ko'chasi,${mahalla} mahallasi, ${tuman} tumani, ${viloyat} viloyati `;
+// console.log(natija);
+
+//#2
+// let kocha="Bog'bon";
+// let mahalla="Sag'bon";
+// let tuman="Bodomzor";
+// let viloyat="Samarqand";
+// let natija=`${kocha} ko'chasi,\n${mahalla} mahallasi, \n${tuman} tumani,\n ${viloyat} viloyati `;
+// console.log(natija);
+
+//#3 trim--boshdan va oxiridan bushliqlarni olish
+// let kocha="         Bog'bon";
+// let mahalla="Sag'bon";
+// let tuman="Bodomzor";
+// let viloyat="Samarqand";
+// let manzil=`${kocha} ko'chasi,${mahalla} mahallasi, ${tuman} tumani, ${viloyat} viloyati.         `;
+// console.log(manzil.trim());
+
+//#4  //Boshdan bushliq olish trimEnd() --oxiridan olish
+// let kocha="         Bog'bon";
+// let mahalla="Sag'bon";
+// let tuman="Bodomzor";
+// let viloyat="Samarqand";
+// let natija=`${kocha} ko'chasi,${mahalla} mahallasi, ${tuman} tumani, ${viloyat} viloyati. `;
+// console.log(natija.trimEnd());//trimEnd()--oxiridan
+
+//#5-- hammasini katta yoki kichik qilish string
+// let kocha="Bog'bon";
+// let mahalla="Sag'bon";
+// let tuman="Bodomzor";
+// let viloyat="Samarqand";
+// let natija=`${kocha} ko'chasi,${mahalla} mahallasi, ${tuman} tumani, ${viloyat} viloyati. `;
+// console.log(natija.toUpperCase());//katta
+// console.log(natija.toLowerCase());//kichik
+
+//#6 //replace(' ',' ')- urniga boshqa suz quyish
+// let text='Assalomu alaykum xurmatli talabalar !';
+// console.log(text.replace('talabalar','ustozlar'));
+
+//#7 string uzunligi nechta belgi borligini aniqlash
+// let kocha="Bog'bon";
+// let mahalla="Sag'bon";
+// let tuman="Bodomzor";
+// let viloyat="Samarqand";
+// let natija=`${kocha} ko'chasi,${mahalla} mahallasi, ${tuman} tumani, ${viloyat} viloyati. `;
+// console.log(natija.length);
+
+//#8 // text ga boshqa text qushish
+// let kocha="Bog'bon";
+// let mahalla="Sag'bon";
+// let tuman="Bodomzor";
+// let viloyat="Samarqand";
+// let natija=`${kocha} ko'chasi,${mahalla} mahallasi, ${tuman} tumani, ${viloyat} viloyati. `;
+// let text='da Yashayman.!!!'
+// console.log(natija.concat(text));
+
+//#9 slice -- belgilangan index buyicha qirqib olish
+// let kocha="Bog'bon";
+// let mahalla="Sag'bon";
+// let tuman="Bodomzor";
+// let viloyat="Samarqand";
+// let natija=`${kocha} ko'chasi,${mahalla} mahallasi, ${tuman} tumani, ${viloyat} viloyati. `;
+// console.log(natija.slice(0,20));
+
+//#10 split stringdan arry xolatiga utqazish (',')
+// let kocha="Bog'bon";
+// let mahalla="Sag'bon";
+// let tuman="Bodomzor";
+// let viloyat="Samarqand";
+// let natija=`${kocha} ko'chasi,${mahalla} mahallasi, ${tuman} tumani, ${viloyat} viloyati. `;
+// console.log(natija.split(","));
+
+
+
+
+
+
+
