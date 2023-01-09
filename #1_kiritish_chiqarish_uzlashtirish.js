c9333d7d0daa9409d4ce1ea5e1d1db6e5be423a3
Shohboz Xurramov <117597489+ShokhbozKh@users.noreply.github.com>
2023-01-09 21:57:10 +0500

    Add files via upload

---
+++ #1_kiritish_chiqarish_uzlashtirish.js
@@ -0,0 +1,248 @@
+/*40 ta masala 03.01.2023 Masalalar yechishda 
+imkon qadar har xil yo'llardan
+foydalanilgan */ //ko'p qatorli izox va // bir qatorli izox
+
+//1
+// let a=4,p;
+// p=4*a;
+// console.log("p=",p);
+
+//2
+// let a=6;
+// s=a*a;
+// console.log(s);
+
+//3
+// let a=3,b=5,p,s;
+// p=2*(a+b);
+// s=a*b;
+// console.log("p:",p,"s:",s);
+
+//4
+// let d=24,l;
+// const pi=3.1415;
+// l=pi*d; 
+// console.log(l);
+
+//5
+// let a=6,v,s;
+// v = a**3 ; s=6*(a**2);
+// console.log("v=",v,"\ns=",s);
+
+//6
+// let a=1,b=2,c=3,v,s;
+// v=a*b*c;
+// s=2*(a*b+b*c+a*c);
+// console.log(s);
+// console.log(v);
+
+//7
+// let r=4,l,s;
+// const pi=3.14;
+// l=2*pi*r;
+// s=pi*(r**2);
+// console.log(l);
+// console.log(s);
+
+//8
+// let a=24,b=12;
+// natija=(a+b)/2;
+// console.log(natija);
+// console.log(typeof(natija));
+
+//9
+// let a=9,b=9;
+// geo= (a*b) **0.5;
+// console.log(geo);
+
+//10
+// var a=12,b=24;
+// sum=a+b;
+// menus=a-b;
+// kv1= a**2;
+// kv2=b*b;
+// console.log(`${sum}\n ${menus} \n ${kv1} \n${kv2}`);
+
+//11
+// let a=1,b=-5;
+// kup=a*b; sum=a+b;
+// a=Math.abs(a);
+// b=Math.abs(b);
+// console.log('kupaytma:',kup);
+// console.log("summa:",sum);
+// console.log(`a modul: ${a} \n b modul:${b}`);
+
+//12
+// const a=4, b=3;
+// c=Math.sqrt(a**2 + b**2);
+// p=a+b+c;
+// console.log(`c=${c}\n p=${p}`);
+
+//13
+/*const r1=6, r2=4;
+s=Math.PI*(r1-r2);
+console.log(s); */
+
+//14
+// const l=324;
+// r=l/2*Math.PI ;
+// s=Math.PI*r*r;
+// console.log(`r=${r}\n s=${s}`);
+
+//15
+// const s=346;
+// r=Math.sqrt(s/Math.PI);
+// d=2*r;
+// console.log(`r=${r} \n d=${d}`);
+
+//16
+// const l1=24,l2=46;
+// masofa=Math.PI*(l2-l1);
+// console.log(masofa);
+
+//17
+// let a=12,b=33;c=45;
+// ac=Math.abs(c-a);
+// bc=Math.abs(b-c);
+// console.log(`ac=${ac} \n bc=${bc} \n yig'indi=${ac+bc}`);
+
+//18
+// let a=12,b=33;c=45;
+// ac=Math.abs(c-a);
+// bc=Math.abs(b-c);
+// console.log(`ac=${ac} \n bc=${bc} \n kupaytma=${ac*bc}`);
+
+//19
+// let a1=1,b1=4;
+// const a2=3, b2=6;
+// a=a2-a1; b=b2-b1;
+// p=(a+b)*2; s=a*b;
+// console.log(`peremetr=${p}\n yuzi=${s}`);
+
+//20
+// let x1=1,x2=4,y1=4,y2=8;
+// m=Math.sqrt((x2-x1)**2 +(y2-y1)**2);
+// console.log("masofa:",m);
+
+//21
+// let x1=1,x2=2,x3=3,y1=5,y2=4,y3=7;
+// a=Math.sqrt((x2-x1)**2 + (y2-y1)**2);
+// b=Math.sqrt((x3-x2)**2 + (y3-y2)**2);
+// c=Math.sqrt((x3-x1)**2 + (y3-y1)**2);
+// p=(a+b+c)/2;
+// s=Math.sqrt(p*(p-a)*(p-b)*(p-c));
+// console.log(`peremetr=${p}\n yuzi=${s}`);
+
+//22
+// let A=4,B=8;
+// A1=A;
+// B1=B;
+// A=B1;
+// B=A1;
+// console.log(`A=${A}\n B=${B}`);
+
+//23
+// let A=1,B=2,C=3;
+// A1=A;
+// B1=B;
+// C1=C;
+// B=A1;
+// C=B1;
+// A=C1;
+// console.log('A=',A," B=",B," C=",C);
+
+// //24
+// let A=1,B=2,C=3;
+// C1=C
+// B1=B;
+// A1=A;
+// C=A1;
+// B=C1;
+// A=B1;
+// console.log(`A=${A}\n B=${B} \nC=${C}`);
+
+//25
+// const x=6;
+// y=3*x**6 -6*x**2 -7;
+// console.log(y);
+
+//26
+// const x=4;
+// y=4*(x-3)**6 - 7*(x-3)**3 + 2;
+// console.log('y=',y);
+
+// //27
+// let a=4;
+// A2=a**2; A4=a**4; A8=a**8;
+// console.log("A^2=",A2,"\n A^4=",A4,"\nA^8=",A8);
+
+//28
+// const a=2;
+// console.log(`A^2=${a**2}\n A^3=${a**3}\n A^5=${a**5}\n A^10=${a**10}\n A^15=${a**15}`);
+
+//29
+// const gr=360;
+// pi=(gr/180)*Math.PI;
+// console.log('pi:',pi);
+
+//30
+// const a=Math.PI*4;
+// gr=(a/Math.PI) * 180;
+// console.log(`Gradus: ${gr} gardus`);
+
+//31
+// const Tc=60;
+// Tf=(Tc*(9/5)+32);////tc9/5 +32
+// console.log("Selsey:",Tf,'frangeyt');
+
+//32
+// const Tf=60;
+// Tc=(Tf-32)*(5/9);
+// console.log("Selsey:",Tc,'gradus');
+
+//33
+// let x=4,a=3400,y=4;
+// console.log(`1 kg konfet:${a/x} sum\n ${y} kg konfet:${(a/x)*y} sum `);
+
+//34
+// let x=2,a=1200;
+// let y=3;
+// a2=(a/x)*y;
+// console.log(`${y}-kg konfet ${a2} sum turadi`);
+
+//35
+// let v1=32,u1=2,t1=4,t2=6;
+// s=t1*(v1+u1)-t2*(v1-u1);
+// console.log(`natija=`,Math.abs(s));
+
+//36
+// let v1=32,v2=46,s=3400,t=24;
+// masofa=s+t*(v1+v2);
+// console.log(masofa);
+
+//37
+// const v1=60,v2=32,s=2400,t=6;
+// masofa=s-t*(v1+v2);
+// console.log('qolgan masofa=',masofa);
+
+//38
+// let a=3,b=4;
+// x=-b/a;
+// console.log(`x=${x}`);
+
+//39
+// const a=2 , b=2,c=-8;
+// x1=(-b-Math.sqrt(b**2-4*a*c))/2*a;
+// x2=(-b+Math.sqrt(b**2-4*a*c))/2*a;
+// console.log('x1=',x1);
+// console.log("x2=",x2);
+
+//40
+// let a1=1,b1=2,c1=3;
+// const a2=4,b2=5,c2=6;
+// d=a1*b2-a2*b1;
+// x=(c1*b2-c2*b1)/d;
+// y=(a1*c2-a2*c1)/d;
+// console.log(`x=${x}`);
+// console.log("y=",y);
+
