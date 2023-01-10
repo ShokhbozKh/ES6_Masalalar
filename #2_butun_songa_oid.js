cc6eb2d3e3e157bb5b264b044927ce059165e48b
Shohboz Xurramov <117597489+ShokhbozKh@users.noreply.github.com>
2023-01-10 10:37:07 +0500

    Add files via upload

---
+++ #2_butun_songa_oid.js
@@ -0,0 +1,160 @@
+//30 ta masala 03.01.2023
+//#1
+// const l=2400;
+// metr=l/100;
+// console.log(metr);
+
+//#2
+// let m=3456;
+// t=m/1000;
+// console.log(`tonna=${Math.round(t)} \n kg=${m%1000}`)
+
+//#3
+// let fayl=2450;
+// hajm=fayl/1024;
+// console.log(hajm);
+
+//#4
+// let A=12,B=5;
+// n=A/B;
+// console.log(Math.floor(n),' marta');
+
+//#5
+// let a=34;b=12;
+// n=Math.floor(a/b);
+// m=Math.floor(a%b);
+// console.log(`joylashadi ${n} marta \n ortadi=${m} sm`);
+
+//#6
+// let number=27;
+// one=number%10;
+// ten=number/10;
+// console.log(`unlar xonasi=${Math.floor(ten)}\n birlar=${one}`);
+
+//#7
+// let number=45;
+// console.log(`raqamlari yig'indisi=${Math.floor(number/10)+number%10}`);
+
+//#8
+// let number=57;
+// number2=`${number%10}`+Math.floor(number/10);
+// console.log(number2);
+
+//#9
+// let number=479;
+// oneH=Math.floor(number/100);
+// console.log(oneH);
+
+// //#10
+// let n=357;
+// one=n%10;
+// ten=Math.floor((n%100)/10);
+// console.log(`birlar=${one}\n unlar=${ten}`);
+
+//#11
+// let number=345;
+// console.log(Math.floor(number/100) + Math.floor((number%100)/10) + number%10);
+
+//#12
+// let number =234;
+// console.log(`${number%10}${Math.floor((number%100)/10)}${Math.floor(number/100)}`);
+
+//#13
+// let n=456;
+// console.log(`${n%100}${Math.floor(n/100)}`);
+
+//#14
+// let n=567;
+// console.log(`${n%10}${Math.floor(n/10)}`);
+
+//#15
+// let n=123;
+// console.log(`${Math.floor((n%100)/10)}${Math.floor(n/100)}${n%10}`);
+
+//#16
+// let n=123;
+// console.log(`${Math.floor(n/100)}${n%10}${Math.floor((n%100)/10)}`);
+
+//#17
+// let n=6789;
+// m=Math.floor(n/100)%10; 
+// console.log(m);
+
+//#18
+// let a=1234;
+// console.log( Math.floor(a/1000)%10 );
+
+//#19
+// let n=24000;
+// minut=Math.floor(n/60);
+// console.log(minut);
+
+//#20
+// let n=60000;
+// hours=Math.floor(n/3600);
+// console.log(hours);
+
+//#21
+// let n=456700;
+// minut=Math.floor(n/60);
+// sekund=n-minut*60;
+// console.log("kun boshidan ",minut, ' minut va ',sekund,' sekund utdi');
+
+//#22
+// let n=45670;
+// hours=Math.floor(n/3600);
+// sekund=n-hours*3600;
+// console.log('soat:',hours,'\n sekund:',sekund);
+
+//#23
+// let n=36061;
+// soat=Math.floor(n/3600);
+// minut=Math.floor((n-(soat*3600))/60);
+// sekund=n - (soat*3600+minut*60);
+// console.log(`Kun boshidan ${soat} soat ${minut} minut ${sekund} sekund utdi`);
+
+//#24
+//1y--1 dush
+//2y--2 sesh
+//3y--3 corshan
+//9 y--2 sesh
+// let k=7;
+// let day=k%7 ;
+// console.log('xafta kuni  : ',day);
+
+// #25
+// 1=d-
+// 1 yanvar--4 payshba
+// 2 yanvar--5 juma
+// 6=sh-
+// let k1=1;
+// day= (k1+3)%7;
+// console.log("kun: ",day);
+
+//#26
+// let k=8;
+// let day=k%7 + 1 ;
+// console.log('xaftani: ',day ,' kuni');
+
+//#27
+// let k=1;
+// day=k%7+6;
+// console.log(day);
+
+//#28
+// let k=1,n=3;
+// let day=((k+n-2))%7+1;
+// console.log("hozirgi kun:",day);
+
+//#29
+// const a=3,b=5,c=2;
+// turtBurchak=a*b;
+// kvadrat=c*c;
+// n=Math.floor(turtBurchak/kvadrat);
+// console.log(`${n} marta joylashadi \n ortgan qisim=${turtBurchak-(n*kvadrat)}`);
+
+//#30
+// let y=1901;
+// n=Math.floor(y/100) +1;
+// console.log(n);
+
