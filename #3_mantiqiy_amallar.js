69b504ddd447ea7f55a84f34d8b6a0e05b4c26ac
Shohboz Xurramov <117597489+ShokhbozKh@users.noreply.github.com>
2023-01-11 18:39:36 +0500

    Add files via upload

---
+++ #3_mantiqiy_amallar.js
@@ -0,0 +1,140 @@
+//Mantiqiy amallarga oid masalalar
+
+//#1
+// let a=8;
+// console.log(Boolean(a>0));
+
+//#2
+// let a=-39;
+// console.log(Boolean(Math.abs(a%2)==1));
+
+//#3
+// const a=-6;
+// n=Boolean(Math.abs(a%2)==0);
+// console.log(n);
+
+//#4
+// let a=4,b=-1;
+// n=(a>2 && b<=3);
+// console.log(Boolean(n));
+
+//#5
+// const a=3,b=-3;
+// n=(a>=0 || b<-2);
+// console.log(Boolean(n));
+
+//#6
+// let a=2,b=3,c=5;
+// n=(a<=b && b<=c);
+// console.log(Boolean(n));
+
+//#7
+// let a=4,b=5,c=7;
+// n=(a<=b && b<=c);
+// console.log(Boolean(n));
+
+//#8
+// let a=5,b=-7;
+// console.log(Boolean((Math.abs(a%2)==1)==(Math.abs(b%2)==1)));
+
+//#9
+// const a=22, b=9;
+// n=Boolean((Math.abs(a%2)==1) || (Math.abs(b%2)==1));
+// console.log(n);
+
+//#10
+// const a=1,b=4;
+// n=Boolean(Math.abs(a%2)==1 && (Math.abs(b%2)==0));
+// console.log(n);
+
+//#11
+
+
+//#12
+
+//#13
+
+//#14
+
+//#15
+
+//#16
+
+//#17
+
+//#18
+
+//#19
+
+//#20
+
+//#21
+
+//#22
+
+//#23
+
+//#24
+
+//#25
+
+
+//#26
+
+//#27
+
+//#28
+
+//#29
+
+//#30
+
+//#31
+// let a=2,b=4,c=1;
+// let n=(a==b) || (a==c)|| (b==c);
+// console.log(n);
+
+//#32
+// let a=5,b=3,c=4;
+// let n=(a**2+b**2==c**2) || (a**2+c**2==b**2)||(c**2+b**2==a**2);
+// console.log(n);
+
+//#33
+// let a=2,b=1,c=1;
+// let n=(a+b>c)||(a+c>b)||(b+c>a);
+// console.log(n);
+
+//#34
+// let x2=1,y2=6;
+// n=(x2%2==1) && (y2%2==0) ||(x2%2==0 && y2%2==1);
+// console.log(n);
+
+//#35
+// let x1=1,x2=5,y1=1,y2=3;
+// n=(x1+y1)%2==0 && (x2+y2)%2==0;
+// console.log(n);
+
+//#36
+// let x1=1,y1=3,x2=6,y2=3;
+// n=Math.abs(x1-x2)==0 || Math.abs(y1-y2)==0
+// console.log(n);
+
+//#37
+// let x1=2,x2=3,y1=4,y2=5;
+// n=Math.abs((x1-x2==0)||(y1-y2==0) ) || (Math.abs((x1-x2)==(y1-y2)));
+// console.log(Boolean(n));
+
+//#38
+// let x1=1,x2=4,y1=1,y2=3;
+// n=(Math.abs(x1-x2)==Math.abs(y1-y2));
+// console.log(n);
+
+//#39
+// let x1=1,y1=1,x2=3,y2=2;
+// n=(Math.abs(x1-x2)==0 || Math.abs(y1-y2)==0) || (x1+y1)==(x2+y2);
+// console.log(n);
+
+//#40
+// let x1=1,y1=2,x2=2,y2=4;
+// n=(Math.abs(x1-x2)==1|| Math.abs(y1-y2)==1)&&(Math.abs(x1-x2)==2 || Math.abs(y1-y2)==2) ;
+// console.log(n);
