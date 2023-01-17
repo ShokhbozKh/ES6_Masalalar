cce3cda140a5802cf017c916624a8de9f278e7a7
Shohboz Xurramov <117597489+ShokhbozKh@users.noreply.github.com>
2023-01-17 20:17:40 +0500

    Add files via upload

---
+++ string2.js
@@ -0,0 +1,13 @@
+let kocha="bog'bon";
+let mahalla="     Sog'bon  ";
+let tuman="   bodomzor  ";
+let viloyat="samarqand";
+// console.log(kocha.length);////uzunligi
+// console.log(mahalla.trim());////bush joylarni olib tashaydi
+// console.log(tuman.toUpperCase());////Hammasini katta qiladi
+// console.log(tuman.toLowerCase());////Hammasini kichik qiladi
+// console.log(viloyat.indexOf('and'));////belgi boshlanishini topib beradi bulmasa -1 chiqaradi.
+// console.log(kocha.concat(" eng zur kucha"));////text ga text qushish
+// console.log(mahalla.includes('q'));//belgini qidiradi bula yoki bulmasa natija boolean qaytadi true or false
+// console.log(tuman.trimStart())//boshlanishdagi bushliq trimEnd()-bu oxiri
+// console.log(viloyat.slice(1));// indexdan boshlab qolganlarni chiqaradi
