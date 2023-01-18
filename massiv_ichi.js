31b426e0a2e813012624f79aa55058a58e38527f
Shohboz Xurramov <117597489+ShokhbozKh@users.noreply.github.com>
2023-01-18 20:03:06 +0500

    Add files via upload

---
+++ massiv_ichi.js
@@ -0,0 +1,64 @@
+
+//#a
+// let a=[1,2,3,4,5];
+// sum=0;
+// for(let i=0;i<a.length;i++){
+//     sum+=a[i];
+// }console.log(sum);
+
+//#b
+// let a=[2,3,4,5,1,7,8];
+// y=1;
+// for(let i=0;i<a.length;i++){
+//     y*=a[i];
+// }console.log(y);
+
+//#c
+// let a=[-2,3,4,-7,4,7,-12];
+// sum=0;
+// for(let i=0;i<a.length;i++){
+//     sum+=Math.abs(a[i]);
+// }
+// console.log(sum);
+
+//#d
+// let a=[1,-3,-5,-7,8,4];
+// y=1;
+// for(let i=0;i<a.length;i++){
+//     y*=Math.abs(a[i]);
+// }console.log(y);
+
+//#e
+// let a=[1,5,2,-5,3,0];
+// sum=0;
+// for(let i=0;i<a.length;i++){
+//     sum+=Math.pow(a[i],2);
+// }
+// console.log(sum);
+
+//#f
+// let a=[1,2,3,4];
+// sum=0,y=1;
+// for(let i=0;i<a.length;i++){
+//     sum+=a[i];
+//     if(i%2==0){
+//         y*=a[i];
+//     }
+// }console.log('s=',sum,'\nkupaytma:',y);
+
+//#g
+// let a=[1,2,3,4,5,6];
+// let n=3;
+// sum=0;
+// for(let i=0;i<n;i++){
+//     sum+=((-1)**(n-1)) * a[i]; 
+// }console.log(sum);
+
+//#h
+// let n=4;
+// let a=[1,2,3,4,5,6,7];
+// f=1;sum=0;
+// for(let i=0;i<2;i++){
+//     sum+=(Math.pow(-1,n-1)*a[i])/(f*=a[i]);
+// }console.log(sum);
+
