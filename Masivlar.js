3c8bad2e576d2f24c9fc534942d25051c2e74cf5
Shohboz Xurramov <117597489+ShokhbozKh@users.noreply.github.com>
2023-01-20 21:39:00 +0500

    Add files via upload

---
+++ Masivlar.js
@@ -0,0 +1,78 @@
+//#1
+// let a=[1,5,3,7];
+// console.log(a);
+
+//#2
+// let a=[1,2,4,8,16,32];
+// console.log(a);
+
+//#3
+// let n=4,a=2,d=2;
+// let N=[2,4,6,8];
+// for(let i=0;i<n;i++){
+//     console.log(N[i]);
+// }
+
+//#4
+// let n=3,a=2,d=2;
+// let N=[2,4,6,8];
+// console.log(N);
+
+//#5
+// a=[1,2,3,5,8,9,10,11,12,13,14];
+// for(let i=1;i<a.length;i++){
+    
+//     if(a[i]==a[i-1]+a[i-2]){
+//         console.log('fibonachi:',a[i],a[i-1],a[i-2]);
+//     }
+// }
+
+//#6
+// let a=[1,2,3,5,11,3,4,7];
+// sum=0;
+// for(let i=0;i<a.length;i++){
+//     if(sum==a[i]){
+//         console.log(`natija:`,a[i]);
+//     }
+//     sum+=a[i];
+// }
+
+//#7
+// let a=[1,2,3,4,5,6,7,8,9];
+// // //console.log(a.reverse());
+// b=[];
+// for(let i=a.length-1;i>=0;i--){
+//     b.push(a[i]);
+// }console.log(b);
+
+//#8
+// let a=[4,5,7,8,6,9];
+// t=0;
+// for(let i=0;i<a.length;i++){
+//     if(a[i]%2!=0){
+//         t++;
+//         console.log(a[i]);
+//     }
+// }console.log(`toqlar soni:${t}`);
+
+//#9
+// let a=[4,5,7,8,6,9];
+// t=0;
+// for(let i=a.length;i>=0;i--){
+//     if(a[i]%2==0){
+//         t++;
+//         console.log(a[i]);
+//     }
+// }console.log(`juftlari soni:${t}`);
+
+//#10//////////////////////////////////////
+// let a=[1,2,3,4,6,8,7,9,33];
+// for(let i=0;i<a.length;i++){
+//     if(a[i]%2==0){
+//         console.log('juftlari',a[i]);
+//     }
+//     if(a[i]%2!=0){
+    
+//         console.log('toqlari',a[i]);
+//     }
+// }
