62ef0f7b06c1294515b9ae35855c414bbbb230a1
Shohboz Xurramov <117597489+ShokhbozKh@users.noreply.github.com>
2023-01-24 23:25:22 +0500

    Add files via upload

---
+++ Masivlar2.js
@@ -0,0 +1,76 @@
+//#11
+// let mas=new Array(1,2,4,7,8,9,0,6);
+// let k=2;
+// for(let i=1;i<mas.length;i++){
+//     if(i%k==0){
+//         console.log(mas[i]);
+//     }
+// }
+
+//#12
+// let massiv=new Array(1,3,5,6,3,8);
+// for(let i=0;i<massiv.length;i=i+2){
+//     console.log(massiv[i]);
+// }
+
+//#13
+// let n=5;
+// let mas=new Array(6,5,7,2,3);
+// for(let i=mas.length-1;i>=0;i=i-2){
+//     console.log(mas[i]);
+// }
+
+//#14
+// let n=6;
+// let massiv=new Array(1,2,3,4,5,6);
+// for (let i=0;i<n;i+=2){
+//     console.log("juft index :",massiv[i]);
+// }
+// for(let i=1;i<n;i+=2){
+//     console.log('Toq index :',massiv[i]);
+// }
+
+//#15
+// let n=6;
+// let mas=new Array(1,3,5,7,3,8);
+// for(let i=1;i<=n;i+=2){
+//     console.log("toq index ",i,mas[i]);
+// }
+// for(let i=n-2;i>=0;i-=2){
+//     console.log("juft index:",i,mas[i]);
+// }
+
+//#16
+// let n=[1,2,3,4,5,6,7,1,0];
+
+// for(let i=0;i<n.length;i++){
+//     console.log(n[i],' ',n[n.length-i]);
+// }
+
+//#17/////////////
+
+////2.massiv elementlarini taxlil qilish
+//#18
+// let n=[7,3,5,6,3,2,8,9];
+// if(n[n.length-1]>n[0]){
+//     console.log(n[0]);
+// }else{
+//     console.log(0);
+// }
+
+//#19
+// let n=new Array(2,1,4,3,6,5);
+// k=3
+// if(n[n.length-1]> n[k] && n[k]>n[0]){
+//     console.log(n.length-1);
+// }else{
+//     console.log(0)
+// }
+
+//#20
+// let k=4,l=7;
+// sum=0;
+// let n=new Array(1,k,6,3,l,9,3,2,4,5,6);
+// for(let i=k;i<=l;i++){
+//     sum+=n[i];
+// }console.log(sum);
